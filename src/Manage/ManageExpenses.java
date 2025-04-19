aef61bc79b7fb190237236cc57fe3a89611ee824
Lucas <Lucasdeapool@gamil.com>
2025-04-19 14:25:59 -0300

    adding expenses

--- src/Manage/ManageExpenses.java
+++ src/Manage/ManageExpenses.java
@@ -2,12 +2,24 @@ package Manage;
 import expenses.Expense;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class ManageExpenses {
-    List<Expense> expensive =  new ArrayList<>();
+
+    Scanner sc = new Scanner(System.in);
+    List<Expense> expensive = new ArrayList<>();
 
     public void addExpense(){
-        System.out.println("funcionando");
+        System.out.println("Welcome to the area to add expenses");
+        System.out.println("please write the value: ");
+        Double value = sc.nextDouble();
+        sc.nextLine();
+        System.out.println("write the description: ");
+        String description = sc.nextLine();
+        System.out.println("Write the category");
+        String category = sc.nextLine();
+        Expense expense = new Expense(value, description,category);
+        expensive.add(expense);
     }
 
     public void listExpense() {
